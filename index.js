882835ef6abd871aea516882720381a55120955c
Sunit Kulkarni <sunitkulkarni@Sunits-MacBook-Pro-2.local>
2017-10-12 20:13:47 -0400

    adding grammatical JSlint modifications to JS code

--- index.js
+++ index.js
@@ -10,20 +10,24 @@ Main Javascript file
 -Sunit
 */
 
-$(function(){
+/* global $ */
+
+$(function () {
     
+    "use strict";
     // Loading ingredients on page load.
-    var ingredients = {}
+    var ingredients = {};
     
     // Get request to cocktail api for all ingredients
     $.ajax({
         url: "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list",
         method: "GET"
-    })
+    });
+    
     .done(function(results){
         //passing results to function to format results
         formatIngredientResults(results);
-    })
+    });
 
     function formatIngredientResults(results){
         //console.log(results.drinks);
