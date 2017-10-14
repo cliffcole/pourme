17fb640a541ca4bb0c8bfec6430562a2040e4012
Sunit Kulkarni <sunitkulkarni@Sunits-MacBook-Pro-2.local>
2017-10-14 11:37:24 -0400

    now working basic render function

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ $(function(){
     });
     
     //get results from random search
-    $('#randomButton').on('submit', function(e){
+    $('#randomButton').on('click', function(e){
         e.preventDefault(); //prevent page from refreshing
         
         var searchUrl = "http://www.thecocktaildb.com/api/json/v1/1/random.php";
@@ -101,8 +101,8 @@ $(function(){
         var searchRandomResult = "";
         searchRandomResult += "<div class='row'>";
         searchRandomResult += "<div class='col-3'>";
-        searchRandomResult += "<a href='#'><img src='"+ result.strDrinkThumb + "' height='200' width='277'></a>"
-        searchRandomResult += result.strDrink;
+        searchRandomResult += "<a href='#'><img src='"+ result.drinks[0].strDrinkThumb + "' height='200' width='277'></a>"
+        searchRandomResult += result.drinks[0].strDrink;
         searchRandomResult += "</div></div>"; //close col div
         
         $('#results-area').append(searchRandomResult);
